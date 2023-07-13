eca810fdc841877c59109fdbe9f2604a1fab44e6
Lakshya123-cyber <lakshyaraikwal85@gmail.com>
2023-07-13 10:57:28 +0800

    final

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,54 +2,49 @@ import "./Footer.css";
 
 const Footer = () => {
   return (
-    <div className="footer">
-      <div className="top">
-        <div className="">
-          <h1>UASG</h1>
-          <p>Uttarakhand Association of Singapore</p>
+    <footer>
+      <div className="footerContainer">
+        <div className="socialIcons">
+          <a href="">
+            <i className="fa-brands fa-facebook"></i>
+          </a>
+          <a href="">
+            <i className="fa-brands fa-twitter"></i>
+          </a>
+          <a href="">
+            <i className="fa-brands fa-instagram"></i>
+          </a>
         </div>
-        <div className="">
-          <a href="/">
-            <i className="fa-brands fa-facebook-square"></i>
-          </a>
-          <a href="/">
-            <i className="fa-brands fa-instagram-square"></i>
-          </a>
-          <a href="/">
-            <i className="fa-brands fa-twitter-square"></i>
-          </a>
+        <div className="footerNav">
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <li>
+              <a href="/event">Events</a>
+            </li>
+            <li>
+              <a href="/Contact">Contact</a>
+            </li>
+          </ul>
         </div>
       </div>
-
-      <div className="bottom">
-        <div className="">
-          <h4>Project</h4>
-          <a href="/">Changelog</a>
-          <a href="/">Status</a>
-          <a href="/">License</a>
-          <a href="/">All Versions</a>
-        </div>
-        <div className="">
-          <h4>Community</h4>
-          <a href="/">Github</a>
-          <a href="/">Issues</a>
-          <a href="/">Project</a>
-          <a href="/">Twitter</a>
-        </div>
-        <div className="">
-          <h4>Help</h4>
-          <a href="/">Support</a>
-          <a href="/">Troubleshooting</a>
-          <a href="/">Contact Us</a>
-        </div>
-        <div className="">
-          <h4>Others</h4>
-          <a href="/">Terms of Service</a>
-          <a href="/">Privacy Policy</a>
-          <a href="/">License</a>
-        </div>
+      <div className="footerBottom">
+        <p>
+          Copyright &copy;2023; Designed and Developed by{" "}
+          <a
+            target="_blank"
+            href="https://raikwal-homepage.vercel.app/"
+            className="dev"
+          >
+            Lakshya Raikwal
+          </a>
+        </p>
       </div>
-    </div>
+    </footer>
   );
 };
 
