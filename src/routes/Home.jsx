eda001fc0da68e6eac68c5a3719734e2b1c80712
Lakshya123-cyber <lakshyaraikwal85@gmail.com>
2024-05-11 15:36:54 +0300

    margin bottom size

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -38,7 +38,7 @@ function Home() {
           width: '100%',
           height: '100%',
           marginTop: '-10px',
-          marginBottom: '-4px',
+          marginBottom: '-2px',
         }}
       />
       <Membership />
